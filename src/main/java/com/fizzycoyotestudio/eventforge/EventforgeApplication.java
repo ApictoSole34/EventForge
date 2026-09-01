06b82a3c53fc4c54ce49d6144a32104ab8d4fd6d
Bartosz Mazur <mazurbartosz99@gmail.com>
2026-09-01 23:40:32 +0200

    initial commit

---
+++ src/main/java/com/fizzycoyotestudio/eventforge/EventforgeApplication.java
@@ -0,0 +1,13 @@
+package com.fizzycoyotestudio.eventforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventforgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fizzycoyotestudio/eventforge/EventforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fizzycoyotestudio.eventforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventforgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
